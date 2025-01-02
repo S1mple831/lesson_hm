c05215958fe024d7b024730ab2fb27b293baf9a9
S1mple831 <1482558794@qq.com>
2025-01-02 21:33:14 +0800

    组件生命周期

---
+++ vue_react/bmw/src/components/LifecycleComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+      <div>
+        <p id="count">Lifecule Component - Count:{{ count }}</p>
+      </div>
+    </div>
+</template>
+
+<script setup>
+import { 
+    defineProps,
+    onBeforeMount,
+    onMounted,
+    onUnmounted,
+    onBeforeUnmount,
+    onBeforeUpdate,
+    onUpdated 
+} from 'vue';
+defineProps({
+    count: {
+        type: Number,
+        required: true
+    }
+});
+
+//console.log('haha');// 在创建的时候执行
+onBeforeMount(() =>{
+    console.log(document.getElementById('count'),'-------')
+    console.log('挂载之前')
+})
+// 组件有生老病死 
+onMounted(() =>{
+   // on 在某个阶段
+   // mounted 挂载完成 
+   // 已经完成了挂载
+    console.log('已经挂载');
+    console.log(document.getElementById('count'),'-------')
+})
+
+onBeforeUnmount(() =>{
+    console.log('卸载之前');
+})
+
+onUnmounted(() =>{
+    console.log('已经卸载');
+})
+
+onBeforeUpdate(() =>{
+    console.log('更新之前');
+})
+
+onUpdated(() =>{
+    console.log('已经更新');
+})
+</script>
+
+<style  scoped>
+
+</style>
---
+++ vue_react/rate/src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+      Main
+      <Item />
+      <Item />
+    </main>
+  </template>
+  
+  <script setup>
+  import Item from './Item.vue';
+  </script>
+  
+  <style scoped>
+  main{
+    flex:1;
+    height: 500px;
+    background-color: yellow;
+  }
+  </style>
