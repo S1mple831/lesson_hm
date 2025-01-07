5881136735f2a367d368992804c8b677c1d02c36
S1mple831 <1482558794@qq.com>
2025-01-07 21:26:15 +0800

    hooks

---
+++ vue_react/vue-hooks/src/components/MousePos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <p>Mouse X: {{ mouse.x }}</p>
+      <p>Mouse Y: {{ mouse.y }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useMouse,useMemo } from '../hooks/useMouse';
+  import Hook from '../hooks/useMouse'
+  const mouse = useMouse();
+  useMemo()
+
+  console.log(Hook);
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ vue_react/vue-hooks/src/components/MousePos2.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <p>Mouse X: {{ mousePos.x }}</p>
+      <p>Mouse Y: {{ mousePos.y }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { 
+    // composition api vue3 核心亮点
+    reactive,
+    onUnmounted,
+    onMounted // 生命周期 life cycle 
+  } from 'vue'
+  // const x = ref(0)
+  // const y = ref(0)
+  
+  const mousePos = reactive({
+    x: 0,
+    y: 0
+  })
+  
+  const mousePosHandler = (e) => {
+    mousePos.x = e.pageX
+    mousePos.y = e.pageY
+    console.log('还在')
+  }
+  // 考虑内存泄漏问题
+  onMounted(() => {
+    window.addEventListener('mousemove', mousePosHandler) 
+  })// 挂载
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', mousePosHandler)
+  })// 卸载
+  </script>
+  
+  <style scoped>
+  
+  </style>
