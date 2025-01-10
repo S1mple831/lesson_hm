cf8dd2edc938ff7562ec96963518f586cd1cbb22
S1mple831 <1482558794@qq.com>
2025-01-10 16:59:30 +0800

    vue项目开发

---
+++ vue_react/v3-element/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {
+  ElButton,
+} from 'element3'
+import 'element3/lib/theme-chalk/index.css'
+import router from './router'
+import {createPinia} from 'pinia'
+
+createApp(App)
+   .use(router)
+   .use(createPinia())
+   .use(ElButton) // 使用UI组件库 开发效率提升了
+   .mount('#app')
